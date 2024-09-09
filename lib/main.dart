d7dcb5e0ea25866bef15eb64bc9cd6970e532951
AdrianKnudsen <adrian@knudsen.no>
2024-09-09 15:03:14 +0200

    update to main.dart
    added weather icon, city name and description of weather

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class WeatherWidget extends StatefulWidget {
 
 class _WeatherWidgetState extends State<WeatherWidget> {
   String weather = 'Loading...';
+  String iconUrl = '';
+  String cityName = 'Bergen City';
 
   @override
   void initState() {
@@ -34,33 +36,45 @@ class _WeatherWidgetState extends State<WeatherWidget> {
     fetchWeather();
   }
 
-Future<void> fetchWeather() async {
-  try {
-    final response = await http.get(Uri.parse(
-      'https://api.openweathermap.org/data/2.5/weather?q=Bergen,no&appid=ca602af39dbd3d74f9b0e5b35212144b&units=metric'
-    ));
+  Future<void> fetchWeather() async {
+    final apiKey = 'ca602af39dbd3d74f9b0e5b35212144b'; 
+    final city = 'Bergen,no';
+    final url = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+
+    final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
+      final weatherDescription = data['weather'][0]['description'];
+      final iconCode = data['weather'][0]['icon'];
+
       setState(() {
-        weather = 'Temperature: ${data['main']['temp']}°C';
+        weather = 'Temperature: ${data['main']['temp']}°C\nDescription: $weatherDescription';
+        iconUrl = 'http://openweathermap.org/img/wn/$iconCode@2x.png'; 
       });
     } else {
       setState(() {
-        weather = 'Failed to load weather data: ${response.statusCode}';
+        weather = 'Failed to load weather data';
+        iconUrl = '';
       });
     }
-  } catch (e) {
-    setState(() {
-      weather = 'Error: $e';
-    });
   }
-}
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(weather),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            cityName,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          if (iconUrl.isNotEmpty)
+            Image.network(iconUrl, width: 100, height: 100),
+          Text(weather, textAlign: TextAlign.center),
+        ],
+      ),
     );
   }
 }
