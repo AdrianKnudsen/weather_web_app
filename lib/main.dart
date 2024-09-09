aa3c240f23b643c98836f1c1497278424dd60c87
AdrianKnudsen <adrian@knudsen.no>
2024-09-09 13:53:34 +0200

    Update main.dart
    added weather api key and some code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +12,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World test'),
+          title: Text('Weather App'),
         ),
-        body: Center(
-          child: Text('Hello, Space!'),
-        ),
+        body: WeatherWidget(),
       ),
+    );
+  }
+}
+
+class WeatherWidget extends StatefulWidget {
+  @override
+  _WeatherWidgetState createState() => _WeatherWidgetState();
+}
+
+class _WeatherWidgetState extends State<WeatherWidget> {
+  String weather = 'Loading...';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWeather();
+  }
+
+Future<void> fetchWeather() async {
+  try {
+    final response = await http.get(Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?q=Bergen,no&appid=ca602af39dbd3d74f9b0e5b35212144b&units=metric'
+    ));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        weather = 'Temperature: ${data['main']['temp']}°C';
+      });
+    } else {
+      setState(() {
+        weather = 'Failed to load weather data: ${response.statusCode}';
+      });
+    }
+  } catch (e) {
+    setState(() {
+      weather = 'Error: $e';
+    });
+  }
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(weather),
     );
   }
 }
